487f4dfa96470e6e12b2b2f0d4e64b12995c9e80
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2022-04-09 12:48:03 +0100

    few changes

--- module/store.js
+++ module/store.js
@@ -1,42 +1,42 @@
-// import { createStore, applyMiddleware } from 'redux'
-// import { persistStore, persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-
-// import rootReducer from './reducers'
-// // import ReduxThunk from 'redux-thunk'
-// import thunkMiddleware from 'redux-thunk'
-// import { createLogger } from 'redux-logger'
-
-// const loggerMiddleware = createLogger()
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// }
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-// const store = createStore(persistedReducer, applyMiddleware( thunkMiddleware,
-//   loggerMiddleware
-//     ))
-//     const persistor = persistStore(store)
-//     export default store
-//     export {persistor}
-
-
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 import rootReducer from './reducers'
-// import { composeWithDevTools} from 'redux-dreevtools-extension'
+// import ReduxThunk from 'redux-thunk'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 
 const loggerMiddleware = createLogger()
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-  ))
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, applyMiddleware( thunkMiddleware,
+  loggerMiddleware
+    ))
+    const persistor = persistStore(store)
+    export default store
+    export {persistor}
+
+
+// import { createStore, applyMiddleware } from 'redux'
+// import rootReducer from './reducers'
+// // import { composeWithDevTools} from 'redux-dreevtools-extension'
+// import thunkMiddleware from 'redux-thunk'
+// import { createLogger } from 'redux-logger'
+
+// const loggerMiddleware = createLogger()
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(
+//     thunkMiddleware,
+//     loggerMiddleware
+//   ))
+
+// export default store
